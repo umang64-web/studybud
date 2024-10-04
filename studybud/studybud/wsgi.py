22786d2b6631df88645a0ddec1e8fa382fb626e7
Umang <umangaggarwal64@gmail.com>
2024-10-04 14:59:36 +0100

    dep change

--- studybud/studybud/wsgi.py
+++ studybud/studybud/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 settings_module = 'studybud.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'studybud.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
+
 
 application = get_wsgi_application()
